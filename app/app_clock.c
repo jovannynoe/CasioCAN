4cc2e7f4bf678cc7d6547a3f11dcd113073970a1
jovannynoe <jovannynoe@gmail.com>
2023-01-05 20:49:24 -0600

    create new files to work

---
+++ app/app_clock.c
@@ -0,0 +1,10 @@
+#include "app_bsp.h"
+#include "app_clock.h"
+
+void Clock_Init( void ){
+
+}
+
+void Clock_Task( void ){
+
+}
---
+++ app/app_clock.h
@@ -0,0 +1,7 @@
+#ifndef __APP_CLOCK_H__
+#define __APP_CLOCK_H__
+
+void Clock_Init( void );
+void Clock_Task( void );
+
+#endif
