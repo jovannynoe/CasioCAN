0c425f895a8de669e40dc01d55ab86a7099618e4
jovannynoe <jovannynoe@gmail.com>
2023-01-16 17:42:35 -0600

    Documentation in BSP header file completed

--- app/app_bsp.h
+++ app/app_bsp.h
@@ -1,3 +1,13 @@
+/**
+ * @file    app_bsp.h
+ * @author  Jovanny Noé Casillas Franco
+ * @brief   **Header file of BSP**
+ *
+ * The BSP header file is to we have a stdint.h library and stm32g0xx header file and we use this header file in all own source files. 
+ * 
+ * @note    We need take in count that this header file only is used in source files.
+ * 
+ */
 #ifndef BSP_H_
 #define BSP_H_
 
