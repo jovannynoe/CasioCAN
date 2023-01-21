82bf08fceb322d8ddb9dd915eaa21fdb23f739d2
jovannynoe <jovannynoe@gmail.com>
2023-01-20 20:54:59 -0600

    Documentation completed in display header file

--- app/app_display.h
+++ app/app_display.h
@@ -1,3 +1,14 @@
+/**
+ * @file    app_display.h
+ * @author  Jovanny Noé Casillas Franco
+ * @brief   **Header file of Display**
+ *
+ * The Display header file is to make the structures and we use in differents files, also, to declarate the functions prototypes
+ * that we are using in the source file.
+ * 
+ * @note    We need take in count that we have the extern with the declaration of structures because we used MISRA.
+ * 
+ */
 #ifndef APP_DISPLAY_H__
 #define APP_DISPLAY_H__
 
